c7115ad34174b31f32ed5932e33ef40e6b33c4fc
Truyen <nguyentruyen28101999@gmail.com>
2020-05-23 20:04:57 +0700

    Second commit: Add some files

--- src/js/game.js
+++ src/js/game.js
@@ -1,7 +1,8 @@
 const startGame = document.querySelector('.start-game');
 const level = document.getElementById('choose-level');
 const startBtn = document.getElementById('start');
-startBtn.addEventListener('click', function() {
+
+startBtn.addEventListener('click', function () {
     startGame.style.display = 'none';
     var g = new Game(level.value);
 });
@@ -10,29 +11,84 @@ class Game {
     constructor(value) {
         this.rows = 0;
         this.cols = 0;
-        this.mineNumber = 0;
-        this.posMine = [];
-        this.setGameBoard(value);
-        this.createGameBoard(this.rows, this.cols)
+        this.rowWidth = 0;
+        this.rowHeight = 0;
+        this.mineNumbers = 0;
+
+        this.setBoard(value);
+        this.createBoard(this.rows, this.cols, this.rowWidth, this.rowHeight);
+        this.randomPosMine(this.mineNumbers, this.rows, this.cols);
+        this.board.addEventListener('click', (e) => {
+            var cell = e.target;
+
+            var row = parseInt(cell.dataset.row);
+            var col = parseInt(cell.dataset.col);
+
+            console.log(row, col);
+
+            if (cell.classList.contains("mine")) {
+                // gameOver(false);
+                console.log('Game over');
+            } 
+        })
     }
 
-    createGameBoard(rows, cols) {
-        console.log(rows, cols);
-    }
-
-    setGameBoard(value) {
+    setBoard(value) {
         if (value === 'Easy') {
-            this.rows = 8;
-            this.cols = 8;
-            this.mineNumber = 10;
+            this.setValue(8, 8, 400, 50, 10);
         } else if (value === 'Medium') {
-            this.rows = 14;
-            this.cols = 18;
-            this.mineNumber = 40;
+            this.setValue(14, 18, 40 * 18, 40, 40);
         } else {
-            this.rows = 20;
-            this.cols = 24;
-            this.mineNumber = 99;
+            this.setValue(20, 24, 30 * 24, 30, 99);
+        }
+    }
+
+    setValue(rows, cols, rowWidth, rowHeight, mineNumbers) {
+        this.rows = rows;
+        this.cols = cols;
+        this.rowWidth = rowWidth;
+        this.rowHeight = rowHeight;
+        this.mineNumbers = mineNumbers;
+    }
+
+    createBoard(rows, cols, rowWidth, rowHeight) {
+        this.board = document.createElement('div');
+        this.board.setAttribute('class', 'board');
+
+        for (let i = 0; i < rows; i++) {
+            var row = document.createElement('div');
+            row.setAttribute('class', 'row');
+            row.style.width = rowWidth + 'px';
+            row.style.height = rowHeight + 'px';
+            for (let j = 0; j < cols; j++) {
+                var col = document.createElement('img');
+                col.setAttribute('class', 'col hidden');
+                col.setAttribute('data-row', i);
+                col.setAttribute('data-col', j);
+                col.src = './src/img/hidden.jpg';
+                col.style.width = rowWidth / cols + 'px';
+                col.style.height = rowHeight + 'px';
+
+                row.appendChild(col);
+            }
+            this.board.appendChild(row);
+        }
+        document.body.appendChild(this.board);
+    }
+
+    randomPosMine(numbers, rows, cols) {
+        var x, y;
+        var cells;
+        for (let i = 0; i < numbers; i++) {
+            x = Math.floor(Math.random() * rows);
+            y = Math.floor(Math.random() * cols);
+            cells = document.querySelectorAll(`[data-row="${x}"][data-col="${y}"]`);
+            if (!cells[0].classList.contains('mine')) {
+                cells[0].classList.add('mine');
+            } else {
+                i--;
+            }
+            console.log(x, y);
         }
     }
 }
