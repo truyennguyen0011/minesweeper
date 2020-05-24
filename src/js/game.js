5fa8ed7875daef61341e355c17c6747b147cdc92
Truyen <nguyentruyen28101999@gmail.com>
2020-05-24 23:21:07 +0700

    Updates right click. Add mine counting function, reveal function and recursive function: helper function

--- src/js/game.js
+++ src/js/game.js
@@ -18,9 +18,24 @@ class Game {
         this.setBoard(value);
         this.createBoard(this.rows, this.cols, this.rowWidth, this.rowHeight);
         this.randomPosMine(this.mineNumbers, this.rows, this.cols);
+
+        this.board.addEventListener('contextmenu', (e) => {
+            var cell = e.target;
+            if (cell.className === 'col') return;
+            if (cell.classList.contains('flag')) {
+                cell.classList.remove('flag');
+                cell.classList.add('hidden');
+                cell.src = './src/img/hidden.jpg';
+            } else {
+                cell.classList.remove('hidden');
+                cell.classList.add('flag');
+                cell.src = './src/img/flag.jpg';
+            }
+        });
         this.board.addEventListener('click', (e) => {
             var cell = e.target;
-
+            if (cell.classList.contains('flag')) return;
+            if (cell.className === 'col') return;
             var row = parseInt(cell.dataset.row);
             var col = parseInt(cell.dataset.col);
 
@@ -28,8 +43,11 @@ class Game {
 
             if (cell.classList.contains("mine")) {
                 // gameOver(false);
+                console.log('++++++++++++++++');
                 console.log('Game over');
-            } 
+            } else {
+                this.reveal(row, col);
+            }
         })
     }
 
@@ -90,6 +108,55 @@ class Game {
             }
             console.log(x, y);
         }
+    }
+
+    reveal(row, col) {
+        this.helper(row, col);
+    }
+
+    helper(i, j) {
+        if (i >= this.rows || j >= this.cols || i < 0 || j < 0) return;
+
+        var cell = document.querySelectorAll(`[data-row="${i}"][data-col="${j}"]`);
+        var mineCount = this.getMineCount(i, j);
+        console.log(mineCount);
+
+        if (!cell[0].classList.contains("hidden") || cell[0].classList.contains("mine")) return;
+
+        cell[0].classList.remove("hidden");
+
+        if (mineCount > 0) {
+            cell[0].src = `./src/img/${mineCount}.jpg`;
+            return;
+        }
+
+        cell[0].src = './src/img/blank.png';
+
+        for (let di = -1; di <= 1; di++) {
+            for (let dj = -1; dj <= 1; dj++) {
+                this.helper(i + di, j + dj);
+            }
+        }
+    }
+
+    getMineCount(i, j) {
+        let count = 0;
+
+        for (let di = -1; di <= 1; di++) {
+            for (let dj = -1; dj <= 1; dj++) {
+                var ni = i + di;
+                var nj = j + dj;
+                if (ni >= this.rows || nj >= this.cols || nj < 0 || ni < 0) {
+                    continue;
+                } else {
+                    var cells = document.querySelectorAll(`[data-row="${ni}"][data-col="${nj}"]`);
+                    if (cells[0].classList.contains("mine")) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
     }
 }
 
